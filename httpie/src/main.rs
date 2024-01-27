86d39d5bb21deb8053480a4d3714336b1ff94711
Haker0428 <513193084@qq.com>
2024-01-27 23:41:58 +0800

    httpie code commit

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,3 +1,169 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Result, anyhow};
+use clap::{Args, Subcommand, Parser};
+use std::str::FromStr;
+use std::collections::HashMap;
+use reqwest::{header, Client, Response, Url};
+use colored::*;
+use mime::Mime;
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{Style, ThemeSet},
+    parsing::SyntaxSet,
+    util::{as_24_bit_terminal_escaped, LinesWithEndings},
+};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// get 
+    Get(Get),
+    /// post
+    Post(Post),
+}
+
+#[derive(Args, Debug)]
+struct Get {
+    /// HTTP 请求的 URL
+    #[arg(value_parser = parse_url)]
+    url: String
+}
+
+#[derive(Args, Debug)]
+struct Post { 
+    /// HTTP 请求的 URL 
+    #[arg(value_parser = parse_url)]
+    url: String, 
+    /// HTTP 请求的 body
+    #[arg(value_parser = parse_kv_pair)]
+    body: Vec<KvPair>, 
+}
+
+/// 命令行中的 key=value 可以通过 parse_kv_pair 解析成 KvPair 结构
+#[warn(dead_code)]
+#[derive(Debug, Clone)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+    Ok(s.into())
+}
+
+// 打印服务器版本号 + 状态码
+fn print_status(resp : &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}", status);
+}
+
+// 打印服务器返回的 HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+
+    println!();
+}
+
+fn print_syntect(s: &str, ext: &str) {
+    // Load these once at the start of your program
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let syntax = ps.find_syntax_by_extension(ext).unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    for line in LinesWithEndings::from(s) {
+        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }
+}
+
+/// 打印服务器返回的 HTTP body
+fn print_body(m: Option<Mime>, body: &str) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
+        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
+
+        // 其它 mime type，我们就直接输出
+        _ => println!("{}", body),
+    }
+}
+
+/// 将服务器返回的 content-type 解析成 Mime 类型
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    print_resp(resp).await?;
+    Ok(())
+}
+
+/// 处理 post 子命令
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    println!("name: {:?}", cli);
+
+    let mut headers = header::HeaderMap::new();
+    // 为我们的 http 客户端添加一些缺省的 HTTP 头
+    headers.insert("X-POWERED-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+
+    let result = match cli.command {
+        Commands::Get(ref args) => get(client, args).await?,
+        Commands::Post(ref args) => post(client, args).await?,
+        _ => (),
+    };
+    Ok(())
 }
